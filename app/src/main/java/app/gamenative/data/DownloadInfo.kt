31fc0672bc2dbbc92e3bfbd6139d29e02d565b88
phobos665 <danielalexanderjoyce@gmail.com>
2026-01-19 11:02:57 +0000

    Removed changes that we don't need for Epic. We should be updating the download in the gog way of doing things.

--- app/src/main/java/app/gamenative/data/DownloadInfo.kt
+++ app/src/main/java/app/gamenative/data/DownloadInfo.kt
@@ -119,7 +119,6 @@ data class DownloadInfo(
             bytesDownloaded = 0L
         }
         addSpeedSample(timestampMs)
-        emitProgressChange()
     }
 
     fun updateStatusMessage(message: String?) {
